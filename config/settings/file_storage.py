d1fea3ddb1688ae0db811f16e528b10b5daf3088
Quiroz <cristianquiroz2311@gmail.com>
2025-01-09 23:53:58 -0300

    Re-Project backend

--- config/settings/file_storage.py
+++ config/settings/file_storage.py
@@ -4,8 +4,8 @@
 
 
 # Configuracion para S3 ---------------------------------------------------------------------------------------------->
-# AWS_ACCESS_KEY_ID = 'AKIAUJ3VUNOLRMPT2SNY'
-# AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = 'AKIAUJ3VUNOLRMPT2SNY'
+AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'bucket-r-projects'
 AWS_S3_SIGNATURE_NAME = 's3v4'
 AWS_S3_REGION_NAME = 'us-east-2'
